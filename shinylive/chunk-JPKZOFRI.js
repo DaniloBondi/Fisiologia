1ada71867660cbf5a011c3f19707baa77f609e47
DaniloBondi <DaniloBondi@users.noreply.github.com>
2026-06-26 13:29:33 +0000

    deploy: c69067e76fed98bad4da80c04976213850d3435a

--- shinylive/chunk-JPKZOFRI.js
+++ shinylive/chunk-JPKZOFRI.js
@@ -1,9 +1,9 @@
-// Shinylive 0.10.9
+// Shinylive 0.10.12
 // Copyright 2026 Posit, PBC
 import {
   __commonJS,
   __toESM
-} from "./chunk-OAXCR3C6.js";
+} from "./chunk-UV7YCL3S.js";
 
 // node_modules/lz-string/libs/lz-string.js
 var require_lz_string = __commonJS({
